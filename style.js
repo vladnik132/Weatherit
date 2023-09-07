4b717e12df7c3e8978550902c89c0157546cba0d
Vladimir <nikolovv27@gmail.com>
2023-09-07 23:14:19 +0300

    Updated

--- style.js
+++ style.js
@@ -3,12 +3,8 @@
 var images = [
     "Backgrounds/bg1.jpg",
     "Backgrounds/bg2.jpg",
+    "Backgrounds/bg3.jpg",
     "Backgrounds/bg4.jpg",
-    "Backgrounds/bg5.jpg",
-    "Backgrounds/bg6.jpg",
-    "Backgrounds/bg7.jpg",
-    "Backgrounds/bg8.jpg",
-    "Backgrounds/bg9.jpg",
   ];
   
   document.getElementsByClassName("app-wrap")[0].style.backgroundImage =
